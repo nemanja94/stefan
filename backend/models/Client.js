07ac310f28b00b48d95976783ea6db13c8b4383e
nemanja94 <aandric.nemanja@gmail.com>
2020-11-25 08:59:35 +0100

    ...

--- backend/models/Client.js
+++ backend/models/Client.js
@@ -9,14 +9,6 @@ const ClientSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     idNumber: {
         type: String,
         required: false
--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -5,6 +5,7 @@ const clientController = require('../controllers/clientController')
 
 router.get('/', clientController.getAll) //Return all clients
 router.post('/getOne', clientController.getOne) //Returns one clinet 
+router.post('/getClientsByName', clientController.getClientsByName) //Returns one clinet 
 router.post('/addOne', clientController.addOne) //Add one client
 router.put('/updateOne', clientController.updateOne) //Updates one clinet
 router.delete('/deleteOne', clientController.deleteOne) //Deletes one clinet
